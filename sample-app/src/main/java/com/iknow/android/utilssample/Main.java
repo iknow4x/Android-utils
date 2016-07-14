566e9fc79c965c9ddad8c3c08f99e833980ea120
Jason <who_know_me@163.com>
2016-07-14 17:56:07 +0800

    add android uitls project

---
+++ sample-app/src/main/java/com/iknow/android/utilssample/Main.java
@@ -0,0 +1,22 @@
+package com.iknow.android.utilssample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.iknow.android.utils.R;
+
+/**
+ * Author：J.Chou
+ * Date：  2016.07.14 17:09.
+ * Email： who_know_me@163.com
+ * Describe:
+ */
+public class Main extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
